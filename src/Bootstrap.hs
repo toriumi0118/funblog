66262fd5fec56ff6b9351f1a60114a4d82390d5a
yasuyuki toriumi <toriumi0118@gmail.com>
2017-01-30 14:25:03 +0900

    Fix: refactor

--- src/Bootstrap.hs
+++ src/Bootstrap.hs
@@ -8,8 +8,10 @@ main :: IO ()
 main = do
   args <- getArgs
   if "-h" `elem` args || "--help" `elem` args
-  then do putStrLn "Usage: funblog"
-          putStrLn ""
-          putStrLn "Configure using the blog.cfg file"
-  else do cfg <- parseConfig "blog.cfg"
-          runBlog cfg
+  then do
+    putStrLn "Usage: funblog"
+    putStrLn ""
+    putStrLn "Configure using the blog.cfg file"
+  else do
+    cfg <- parseConfig "blog.cfg"
+    runBlog cfg
